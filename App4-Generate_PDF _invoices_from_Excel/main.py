39dd6589f0d661d8a4e3087d9593db8f3b25b842
saikr <saikrishna2589@gmail.com>
2024-01-31 18:00:21 +1100

    Load the excel files into data frames

--- App4-Generate_PDF _invoices_from_Excel/main.py
+++ App4-Generate_PDF _invoices_from_Excel/main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-
+from fpdf import fpdf
 # we need to create python list as there are multiple files.
 # so we make use of glob module. point to the folder and the pattern of files to be read
 
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 import glob
 
 file_paths= glob.glob("invoices/*.xlsx")
@@ -10,3 +11,4 @@ file_paths= glob.glob("invoices/*.xlsx")
 for filepath in file_paths:
     df=pd.read_excel(filepath,sheet_name="Sheet 1")
     print(df)
+
