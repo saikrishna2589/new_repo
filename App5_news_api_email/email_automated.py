66479defe22a56a466447e82a32115cdf08b506f
saikr <saikrishna2589@gmail.com>
2024-02-05 18:06:01 +1100

    Day27:Send automated email on API topic

--- App5_news_api_email/email_automated.py
+++ App5_news_api_email/email_automated.py
@@ -1,10 +1,5 @@
-# how to make password more secure using environment variables?
-
-# smptlib is used as an email library that create email client session
-# ssl library is to make the context secure
 import smtplib, ssl
 import os
-
 
 def send_email(message):
     host = "smtp.gmail.com"
@@ -19,27 +14,3 @@ def send_email(message):
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-
-"""
-import smtplib, ssl
-import os
-
-
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
-    username = "appemailtest89@gmail.com"
-    # creating environment variable with os (operating system) library so as to keep password safe
-
-    password = os.getenv("PASSWORD")
-
-    receiver = "appemailtest89@gmail.com"
-
-    context = ssl.create_default_context()
-    with smtplib.SMTP("smtp.gmail.com") as server:
-        server.starttls() and server.login(user=username, password=password)
-        server.sendmail(from_addr=username,
-                        to_addrs=receiver,
-                        msg=message)
-"""
--- App5_news_api_email/main.py
+++ App5_news_api_email/main.py
@@ -1,30 +1,22 @@
-
-#importing requests library to read apis for webpages
 import requests
 from email_automated import send_email
 
-#go to newsapi.org ,login and get the api_key
-api_key="4ab757abe0f042728570a4cfce6d64dc"
-url = ("https://newsapi.org/v2/everything?q=bitcoin&from=2024-01-05&"
-       "sortBy=publishedAt&apiKey"
-       "=4ab757abe0f042728570a4cfce6d64dc")
+api_key = "4ab757abe0f042728570a4cfce6d64dc"
+url = "https://newsapi.org/v2/everything?q=bitcoin&" \
+      "sortBy=publishedAt&apiKey=" \
+      "4ab757abe0f042728570a4cfce6d64dc"
 
-#this is a function that gets url. requests.get function creates a request object type.
+# Make request
 request = requests.get(url)
 
-#this method .txt prints out the content in string format. so type of content is string
-content_text=request.text
+# Get a dictionary with data
+content = request.json()
 
-#however if you want data in more structuted way such as dictionary , we use .json method on request object
-content_dict=request.json()
+# Access the article titles and description
+body = ""
+for article in content["articles"]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
 
-body=""
-
-#getting each value in the list of 100 articles key by accessing the title key within each list
-for article in content_dict["articles"]:
-       if article["title"] is not None:
-              body=body+article["title"]+"\n"+article["description"]+2*"\n"
-
-#convert the string to udf-8 format before sending email
-body=body.encode("utf-8")
+body = body.encode("utf-8")
 send_email(message=body)
