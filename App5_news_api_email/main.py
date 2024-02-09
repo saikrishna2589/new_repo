d4f6060ffc11260a80778a968302a197f7d40bcd
saikr <saikrishna2589@gmail.com>
2024-02-09 17:23:53 +1100

    Day28:Add links, specify language, limit news, add subject and control the topic

--- App5_news_api_email/main.py
+++ App5_news_api_email/main.py
@@ -2,9 +2,17 @@ import requests
 from email_automated import send_email
 
 api_key = "4ab757abe0f042728570a4cfce6d64dc"
-url = "https://newsapi.org/v2/everything?q=bitcoin&" \
-      "sortBy=publishedAt&apiKey=" \
-      "4ab757abe0f042728570a4cfce6d64dc"
+
+topic="tesla"
+
+
+#to the url we are adding '&languge=en' at the end so we only extract english language articles
+#in the documnetation on newsapi.com, endpoints, you will find documentation for language and 'en'
+url =  "https://newsapi.org/v2/everything"\
+       f"?q={topic}&" \
+       "sortBy=publishedAt" \
+       "&apiKey=4ab757abe0f042728570a4cfce6d64dc"\
+       "&language=en"
 
 # Make request
 request = requests.get(url)
@@ -12,11 +20,16 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-# Access the article titles and description
+
+print(content["articles"])
+# Access the article titles and description for first 20 articles as it is a list of 100 articles
 body = ""
-for article in content["articles"]:
+for article in content["articles"][0:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+        body = "Subject: Today's news"\
+        +"\n"+ body + article["title"] \
+        + "\n" + str(article["description"]) \
+        + "\n" + article["url"]+ 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
