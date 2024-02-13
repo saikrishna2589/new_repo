9fabf5a1717a4fe3619ef59db7514a74c31f8b94
saikr <saikrishna2589@gmail.com>
2024-02-13 17:44:37 +1100

    Day29-Student_assignment_part2_user_enter_word_output_definition

--- Build_API_student_project_App6_Day29/main.py
+++ Build_API_student_project_App6_Day29/main.py
@@ -1,6 +1,7 @@
 
 # importing Flask  and render_template
 from flask import Flask,render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,11 +13,16 @@ def home():
 
 
 #with any api word parameter, return the definition of the word and the word itself as dictionary
-@app.route("/api/v1/<word>")
-def word_definition(word):
-    definition =word.upper()
-    result = { "definition":definition,
-             "word":word}
+@app.route("/api/v1/<selected_word>")
+def word_definition(selected_word):
+    filename="dictionary.csv"
+    df= pd.read_csv(filename)
+
+    chosen_row =df.loc[df["word"]==str(selected_word).lower()]
+    meaning= chosen_row["definition"].squeeze()
+    result = { "word":selected_word,"definition":meaning
+
+               }
     return result
 
 
