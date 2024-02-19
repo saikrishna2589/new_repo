578f5e766c8108e70a24232bde9fbc89f5a48083
saikr <saikrishna2589@gmail.com>
2024-02-19 21:41:42 +1100

    day 33-create weather api function and access temperature and weather values

--- App7-Build weather forecast dashboard/backend.py
+++ App7-Build weather forecast dashboard/backend.py
@@ -1,24 +1,19 @@
 
 import requests
+import requests
 
-API_KEY="dbe41059785aea0e58eec0a907efb2c6"
+API_KEY = "141710af2113bab9f55ef73e1bcd33d5"
 
-def get_data(place,forecast_days=None,kind=None):
-   url=f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
-   response=requests.get(url)
-   data=response.json()
-   filtered_data=data["list"]
-   nr_values=8*forecast_days
-   filtered_data=filtered_data[:nr_values]
-   if kind=="Temperature":
-       filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-   if kind=="sky":
-        filtered_data=[dict["weather"][0]["main"] for dict in filtered_data]
-   return filtered_data
 
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
 
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo",forecast_days=3,kind="sky"))
-
-
+    print(get_data(place="Tokyo", forecast_days=3))
--- App7-Build weather forecast dashboard/main.py
+++ App7-Build weather forecast dashboard/main.py
@@ -1,26 +1,35 @@
 import streamlit as st
 import plotly.express as px
-import pandas as pd
 from backend import get_data
 
-st.set_page_config(layout="wide")
-
-st.title("Weather Forecast in the short-term ")
+# Add title, text input, slider, selectbox, and subheader
+st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
-                 help="Select the number of days to be forecasted")
-
+                 help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
-                      ("Temperature", "Weather"))
-st.subheader(f"{option} for next {days} days in {place}")
+                      ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get the temperature/sky data
+    try:
+        filtered_data = get_data(place, days)
 
-data=get_data(place,days,options)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] /10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width=115)
 
-
-# Create a line plot using Plotly Express
-fig = px.line(data, x="Date", y="Temperature", title=f"{option} for next {days} days in {place}")
-
-# Show the Plotly figure using Streamlit
-st.plotly_chart(fig)
+    except KeyError:
+        st.write("That place does not exist")
